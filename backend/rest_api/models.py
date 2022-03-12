f2d8e5278173aa2ef3c9696749d5d9607603de5d
Nikita Radin <NikitaRadin1999@gmail.com>
2022-03-12 13:35:05 +0300

    Добавлен файл моделей

--- backend/rest_api/models.py
+++ backend/rest_api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class AuthenticationCode(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    encrypted = models.CharField(max_length=100)
