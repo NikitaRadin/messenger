c3dea671b8b6cc9bdd4eb16c573844005c2a4e9a
Nikita Radin <NikitaRadin1999@gmail.com>
2022-03-28 17:25:33 +0300

    Добавлена возможность обращения к /login

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -4,75 +4,48 @@ import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
 
-const steps = ['Select campaign settings', 'Create an ad group', 'Create an ad'];
+const steps = ['Enter your username', 'Enter your authentication code'];
 
-export default function HorizontalLinearStepper() {
+function Login() {
     const [activeStep, setActiveStep] = React.useState(0);
-    const [skipped, setSkipped] = React.useState(new Set());
+    const [username, setUsername] = React.useState('');
+    const [userId, setUserId] = React.useState(0);
+    const [alertOpen, setAlertOpen] = React.useState(true);
 
-    const isStepOptional = (step) => {
-        return step === 1;
+    async function sendUsername() {
+        await fetch('http://127.0.0.1:8000/login/', {
+            method: 'POST',
+            body: JSON.stringify({ 'username': username })
+        })
+            .then(response => response.json())
+            .then(
+                (response) => {
+                    setUserId(response.user_id);
+                    setActiveStep(activeStep + 1);
+                },
+                (error) => { }
+            );
     };
 
-    const isStepSkipped = (step) => {
-        return skipped.has(step);
+    function closeAlert() {
+        setAlertOpen(false);
     };
 
-    const handleNext = () => {
-        let newSkipped = skipped;
-        if (isStepSkipped(activeStep)) {
-            newSkipped = new Set(newSkipped.values());
-            newSkipped.delete(activeStep);
-        }
+    function goBack() { };
 
-        setActiveStep((prevActiveStep) => prevActiveStep + 1);
-        setSkipped(newSkipped);
-    };
-
-    const handleBack = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep - 1);
-    };
-
-    const handleSkip = () => {
-        if (!isStepOptional(activeStep)) {
-            // You probably want to guard against something like this,
-            // it should never occur unless someone's actively trying to break something.
-            throw new Error("You can't skip a step that isn't optional.");
-        }
-
-        setActiveStep((prevActiveStep) => prevActiveStep + 1);
-        setSkipped((prevSkipped) => {
-            const newSkipped = new Set(prevSkipped.values());
-            newSkipped.add(activeStep);
-            return newSkipped;
-        });
-    };
-
-    const handleReset = () => {
-        setActiveStep(0);
-    };
+    function sendAuthenticationCode() { };
 
     return (
         <Box sx={{ mt: 30 }}>
             <Container maxWidth="md">
                 <Stepper activeStep={activeStep}>
                     {steps.map((label, index) => {
-                        const stepProps = {};
-                        const labelProps = {};
-                        if (isStepOptional(index)) {
-                            labelProps.optional = (
-                                <Typography variant="caption">Optional</Typography>
-                            );
-                        }
-                        if (isStepSkipped(index)) {
-                            stepProps.completed = false;
-                        }
                         return (
-                            <Step key={label} {...stepProps}>
-                                <StepLabel {...labelProps}>{label}</StepLabel>
+                            <Step key={label}>
+                                <StepLabel>{label}</StepLabel>
                             </Step>
                         );
                     })}
@@ -86,40 +59,30 @@ export default function HorizontalLinearStepper() {
                     alignItems: 'center'
                 }}
             >
-                {activeStep === steps.length ? (
-                    <React.Fragment>
-                        <Typography sx={{ mt: 2, mb: 1 }}>All steps completed - you&apos;re finished</Typography>
-                        <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-                            <Box sx={{ flex: '1 1 auto' }} />
-                            <Button onClick={handleReset}>Reset</Button>
-                        </Box>
-                    </React.Fragment>
-                ) : (
-                        <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
-                    )}
+                <TextField
+                    size='small'
+                    placeholder='Enter your username'
+                    value={username}
+                    onChange={(event) => setUsername(event.target.value)}
+                    sx={{ my: 5 }}
+                />
             </Container>
             <Container maxWidth="md">
                 <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
                     <Button
                         color="inherit"
                         disabled={activeStep === 0}
-                        onClick={handleBack}
+                        onClick={goBack}
                         sx={{ mr: 1 }}
                     >
-                        Back
+                        Go back
                     </Button>
                     <Box sx={{ flex: '1 1 auto' }} />
-                    {isStepOptional(activeStep) && (
-                        <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
-                            Skip
-                        </Button>
-                    )}
-
-                    <Button onClick={handleNext}>
-                        {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-                    </Button>
+                    <Button onClick={sendUsername}>Send</Button>
                 </Box>
             </Container>
         </Box>
     );
-}
+};
+
+export default Login;
