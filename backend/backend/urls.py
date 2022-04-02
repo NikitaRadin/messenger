701fdd9b0bdf1e93d992c82c731c1a463332abfe
Nikita Radin <NikitaRadin1999@gmail.com>
2022-04-02 15:06:52 +0300

    Добавлен Swagger UI

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path, include
 from django.contrib import admin
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('', include('authorization.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('openapi-schema/', get_schema_view(
+        title='Messenger API',
+        description='Messenger API documentation',
+        url='http://127.0.0.1:8000/',
+        version='1.0.0'
+    ), name='openapi-schema'),
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui')
 ]
